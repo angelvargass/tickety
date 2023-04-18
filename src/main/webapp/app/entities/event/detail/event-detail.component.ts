89fce2afe4e00668f86db65660a07dc88abf7571
ppedroza404 <78894120+ppedroza404@users.noreply.github.com>
2023-04-18 11:52:15 -0600

    NavBar Animation
    Added navbar efect

--- src/main/webapp/app/entities/event/detail/event-detail.component.ts
+++ src/main/webapp/app/entities/event/detail/event-detail.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 import { IEvent } from '../event.model';
+import { IPhoto } from '../../photo/photo.model';
+import { IGalery } from '../../galery/galery.model';
 
 @Component({
   selector: 'jhi-event-detail',
@@ -10,15 +12,17 @@ import { IEvent } from '../event.model';
 })
 export class EventDetailComponent implements OnInit {
   event: IEvent | null = null;
+  galery: IGalery | null = null;
 
   constructor(protected activatedRoute: ActivatedRoute) {}
 
   ngOnInit(): void {
     this.activatedRoute.data.subscribe(({ event }) => {
       this.event = event;
+      this.galery = <IGalery>this.event?.galery;
     });
 
-    console.log(this.event);
+    console.log(this.galery?.photos);
   }
 
   previousState(): void {
